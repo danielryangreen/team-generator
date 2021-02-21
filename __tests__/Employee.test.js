6a72035a9cf0b3f824351100bed267bca8e3c3f9
Dan Green <dan.ryan.green@gmail.com>
2021-02-21 13:19:23 -0700

    Uninstall packages

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("@jest/globals");
+const { describe } = require("yargs");
+const Employee = require("../lib/Employee");
+
+describe("Initialization", () => {
+  it("should return an object when called with the 'new' keyword", () => {
+    const employee = new Employee();
+    expect(typeof(employee).toBe("object"));
+  });
+});

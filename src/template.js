7560810923c6f27810a03d76f3bd7b6e59e88ea3
Dan Green <dan.ryan.green@gmail.com>
2021-02-23 21:24:34 -0700

    Playing with indentation in template literals

--- src/template.js
+++ src/template.js
@@ -1,36 +1,29 @@
 function generateHtml(data) {
   return `<p>boilerplate stuff</p>
-  ${generateTeam(data)}
-  `;
+${generateTeam(data)}`;
 }
 
 const generateTeam = team => {
 
   const generateManager = manager => {
-    return `
-    <p>${manager.getName()}</p>
-    <p>${manager.getId()}</p>
-    <p>${manager.getEmail()}</p>
-    <p>${manager.officeNumber}</p>
-    `;
+    return `<p>${manager.getName()}</p>
+<p>${manager.getId()}</p>
+<p>${manager.getEmail()}</p>
+<p>${manager.officeNumber}</p>`;
   };
   
   const generateEngineer = engineer => {
-    return `
-    <p>${engineer.getName()}</p>
-    <p>${engineer.getId()}</p>
-    <p>${engineer.getEmail()}</p>
-    <p>${engineer.getGithub()}</p>
-    `;
+    return `<p>${engineer.getName()}</p>
+<p>${engineer.getId()}</p>
+<p>${engineer.getEmail()}</p>
+<p>${engineer.getGithub()}</p>`;
   };
 
   const generateIntern = intern => {
-    return `
-    <p>${intern.getName()}</p>
-    <p>${intern.getId()}</p>
-    <p>${intern.getEmail()}</p>
-    <p>${intern.getSchool()}</p>
-    `;
+    return `<p>${intern.getName()}</p>
+<p>${intern.getId()}</p>
+<p>${intern.getEmail()}</p>
+<p>${intern.getSchool()}</p>`;
   };
 
   const html = [];
